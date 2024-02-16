dd5378649dc11fd66414d4901059eed3f07455db
HoaiAnT <143848701+HoaiAnT@users.noreply.github.com>
2024-02-16 15:24:36 -0800

    done

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,3 +1,6 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
-console.log(arrays.reduce((a,b) => a.concat(b)));
+function flatten(array){
+    return array.reduce((a,b) => a.concat(b),[]);
+}
+console.log(flatten(arrays))
 // → [1, 2, 3, 4, 5, 6]
--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,7 +1,7 @@
 function loop (value, test, update, body){
-    for (let i = value; test(i); i = update(i)) {
-        body(i)
-      }
+  for (value; test(value);value = update(value)) {
+    body(value);
+  }
 }
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
